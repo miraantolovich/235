7a67419a849ee38bc92379147d4bad90c778f3a6
Mira Antolovich <48299535+mla4783@users.noreply.github.com>
2019-11-22 14:47:53 -0500

    push circleblast

--- HW-pixi-game-start/js/main.js
+++ HW-pixi-game-start/js/main.js
@@ -1,25 +1,25 @@
 // We will use `strict mode`, which helps us by having the browser catch many common JS mistakes
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
 "use strict";
-const app = new PIXI.Application(600,600);
+const app = new PIXI.Application(600, 600);
 document.body.appendChild(app.view);
 
 // constants
 const sceneWidth = app.view.width;
-const sceneHeight = app.view.height;	
+const sceneHeight = app.view.height;
 
 // pre-load the images
 PIXI.loader.
-add(["images/Spaceship.png","images/explosions.png"]).
-on("progress",e=>{console.log(`progress=${e.progress}`)}).
-load(setup);
+    add(["images/Spaceship.png", "images/explosions.png"]).
+    on("progress", e => { console.log(`progress=${e.progress}`) }).
+    load(setup);
 
 // aliases
 let stage;
 
 // game variables
 let startScene;
-let gameScene,ship,scoreLabel,lifeLabel,shootSound,hitSound,fireballSound;
+let gameScene, ship, scoreLabel, lifeLabel, shootSound, hitSound, fireballSound;
 let gameOverScene;
 
 let circles = [];
@@ -33,32 +33,41 @@ let levelNum = 1;
 let paused = true;
 
 function setup() {
-	stage = app.stage;
-	// #1 - Create the `start` scene
+    stage = app.stage;
+    // #1 - Create the `start` scene
     startScene = new PIXI.Container();
     stage.addChild(startScene);
-	// #2 - Create the main `game` scene and make it invisible
+    // #2 - Create the main `game` scene and make it invisible
     gameScene = new PIXI.Container();
     gameScene.visible = false;
     stage.addChild(gameScene);
-	// #3 - Create the `gameOver` scene and make it invisible
-	gameOverScene = new PIXI.Container();
+    // #3 - Create the `gameOver` scene and make it invisible
+    gameOverScene = new PIXI.Container();
     gameOverScene.visible = false;
     stage.addChild(gameOverScene);
-	// #4 - Create labels for all 3 scenes
-	createLabelsAndButtons();
-	// #5 - Create ship
-	
-	// #6 - Load Sounds
-	
-	// #7 - Load sprite sheet
-		
-	// #8 - Start update loop
-	
-	// #9 - Start listening for click events on the canvas
-	
-	// Now our `startScene` is visible
-	// Clicking the button calls startGame()
+    // #4 - Create labels for all 3 scenes
+    createLabelsAndButtons();
+    // #5 - Create ship
+    ship = new Ship();
+    gameScene.addChild(ship);
+    // #6 - Load Sounds
+    shootSound = new Howl({
+        src: ['sounds/shoot.wav']
+    });
+    hitSound = new Howl({
+        src: ['sounds/hit.mp3']
+    });
+    fireballSound = new Howl({
+        src: ['sounds/fireball.mp3']
+    });
+    // #7 - Load sprite sheet
+
+    // #8 - Start update loop
+
+    // #9 - Start listening for click events on the canvas
+
+    // Now our `startScene` is visible
+    // Clicking the button calls startGame()
 }
 
 function createLabelsAndButtons() {
@@ -101,12 +110,12 @@ function createLabelsAndButtons() {
     startButton.interactive = true;
     startButton.buttonMode = true;
     startButton.on("pointerup", startGame); //function reference
-    startButton.on("pointerover", e=>e.target.alpha = 0.7); //concise arrow function
-    startButton.on("pointerout", e=>e.target.alpha = 1); //concise arrow function
+    startButton.on("pointerover", e => e.target.alpha = 0.7); //concise arrow function
+    startButton.on("pointerout", e => e.target.alpha = 1.0); //concise arrow function
     startScene.addChild(startButton);
 
     // 2 - set up game scene
-    let textStyle = new PIXI.textStyle({
+    let textStyle = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 18,
         fontFamily: "Futura",
@@ -128,9 +137,32 @@ function createLabelsAndButtons() {
     gameScene.addChild(lifeLabel);
     decreaseLifeBy(0);
 
-    // 3 - set up game over scene
+    // 3 - set up `gameOverScene`
     // 3A - make game over text
-    let gameOverText = new PIXI.Text("Game Over!\n      :-O");
+    let gameOverText = new PIXI.Text("Game Over!\n        :-O");
+    textStyle = new PIXI.TextStyle({
+        fill: 0xFFFFFF,
+        fontSize: 64,
+        fontFamily: "Futura",
+        stroke: 0xFF0000,
+        strokeThickness: 6
+    });
+    gameOverText.style = textStyle;
+    gameOverText.x = 100;
+    gameOverText.y = sceneHeight / 2 - 160;
+    gameOverScene.addChild(gameOverText);
+
+    // 3B - make "play again?" button
+    let playAgainButton = new PIXI.Text("Play Again?");
+    playAgainButton.style = buttonStyle;
+    playAgainButton.x = 150;
+    playAgainButton.y = sceneHeight - 100;
+    playAgainButton.interactive = true;
+    playAgainButton.buttonMode = true;
+    playAgainButton.on("pointerup", startGame); // startGame is a function reference
+    playAgainButton.on('pointerover', e => e.target.alpha = 0.7); // concise arrow function with no brackets
+    playAgainButton.on('pointerout', e => e.currentTarget.alpha = 1.0); // ditto
+    gameOverScene.addChild(playAgainButton);
 }
 
 function startGame() {
@@ -143,10 +175,48 @@ function startGame() {
 
 function increaseScoreBy(value) {
     score += value;
-    scoreLabel.text = "score ${score}";
+    scoreLabel.text = `Score ${score}`;
 }
 function decreaseLifeBy(value) {
     life -= value;
     life = parseInt(life);
-    lifeLabel.text = "Life ${life}%";
+    lifeLabel.text = `Life ${life}%`;
+}
+
+function gameLoop() {
+    // if (paused) return; // keep this commented out for now
+
+    // #1 - Calculate "delta time"
+    let dt = 1 / app.ticker.FPS;
+    if (dt > 1 / 12) dt = 1 / 12;
+
+    // #2 - Move Ship
+    let mousePosition = app.renderer.plugins.interaction.mouse.global;
+    let amt = 6 * dt; //at 60 fps, will move around 10% of distance
+    //lerp x and y values
+    let newX = lerp(ship.x, mousePosition.x, amt);
+    let newY = lerp(ship.y, mousePosition.y, amt);
+    //keep ship on screen with clamp()
+    let w2 = ship.width/2;
+    let h2 = ship.height/2;
+    ship.x = clamp(newX, 0+w2, sceneWidth-w2);
+    ship.y = clamp(newY, 0+h2, sceneHeight-h2);
+
+    // #3 - Move Circles
+
+    // #4 - Move Bullets
+
+
+    // #5 - Check for Collisions
+
+
+    // #6 - Now do some clean up
+
+
+    // #7 - Is game over?
+
+
+    // #8 - Load next level
+    app.ticker.add(gameLoop);
+
 }
